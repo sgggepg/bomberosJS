d2c234399661a3a3204efd47c959649b4a9809bd
SG <sg@eoss.com.co>
2017-02-13 19:49:43 -0500

    first deploy

---
+++ index.js
@@ -0,0 +1,45 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var redis = require('redis');
+
+var redisServer = {
+    host: 'redis-17428.c10.us-east-1-4.ec2.cloud.redislabs.com',
+    port: 17428
+};
+
+var redisClient = redis.createClient(redisServer);
+redisClient.on("message", function(channel, message) {
+    try {
+        console.log("mew message in queue " + message + "channel" + channel);
+        io.emit('new Message', message);
+    } catch (err) {
+
+    }
+});
+io.on('connection', function(socket) {
+    console.log('user connected');
+    io.emit('user connected');
+    socket.on('new Message', function(msg) {
+        io.emit('new Message', msg);
+    });
+    socket.on('disconnect', function() {
+        redisClient.quit();
+    });
+
+    redisClient.subscribe('message');
+
+});
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+
+});
+
+
+
+
+server.listen(9000, function() {
+    console.log('listening on port *9000');
+});
